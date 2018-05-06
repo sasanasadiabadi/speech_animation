662c4a323a4802bcbf25fea69b208d08c75b0f98
sasan asadiabadi <36397328+sasanasadiabadi@users.noreply.github.com>
2018-05-06 19:58:58 +0300

    Update pca_shape.jl

--- pca_shape.jl
+++ pca_shape.jl
@@ -1,6 +1,7 @@
 # apply dimensionality reduction on data
 
 path = "--path to data"
+save_path = "-- path to saving folder"
 
 #read data from path
 files = readdir(path)
@@ -8,12 +9,11 @@ files = readdir(path)
 X = Any[]
 for k in files
 	dt = readdlm(joinpath(path,k), ',')
-
 	push!(X,dt)
 end
 X = reduce(vcat,X)
 X = X'
-println(size(X))
+println("data size: ", size(X))
 
 # mean substraction
 X_bar = mean(X,2)
@@ -25,7 +25,7 @@ e_val = sort(e_val, rev=true)
 # get minimum number of parameters to explain variation
 tlist = cumsum(e_val).>0.98*sum(e_val)
 t = findfirst(tlist)
-println(t)
+println("number of pca parameters: ", t)
 
 evect = zeros(size(e_vec,1),t)
 for i=1:t
@@ -39,4 +39,10 @@ for i=1:size(X,2)
     X_pca[:,i] = evect'*(X[:,i] - X_bar)
 end
 
-print(size(X_pca))
+# write pca data into csv files
+j = 0
+for k in files
+	filname = joinpath(save_path,k)
+    writecsv(filname, X_pca[:,(j*75)+1:(j+1)*75])
+    j = j + 1
+end
